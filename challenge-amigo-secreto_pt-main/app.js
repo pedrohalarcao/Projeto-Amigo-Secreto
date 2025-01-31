f3de0b7701b5f85e507fe863c84d6f29b334e634
Pedro Alarcão <pedroalarcao2@gmail.com>
2025-01-31 20:44:37 -0300

    Função para agregar amigos implementada.

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,31 @@
 let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    if (amigos.includes(nome)) {
+        alert("Esse nome já foi adicionado");
+        return;
+    }
+
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach(nome => {
+        let li = document.createElement("li");
+        li.textContent = nome;
+        lista.appendChild(li);
+    });
+}
